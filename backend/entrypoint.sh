76ef8488ed2fdfb490d48ae5e93e2218139c279e
thiagoc-machado <thiagocmachado@yahoo.com.br>
2025-07-08 07:51:03 +0200

    deploy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# Espera pelo banco
-echo "Aguardando o banco de dados iniciar..."
+# Mostra mensagens informativas
+echo "🏁 Iniciando entrypoint.sh"
+
+# Espera alguns segundos para garantir que o banco esteja pronto
+echo "⏳ Aguardando banco de dados..."
 sleep 5
 
-# Roda migrations e coleta arquivos estáticos
+# Executa as migrações
+echo "🔄 Rodando migrações..."
 python manage.py migrate --noinput
+
+# Coleta arquivos estáticos
+echo "📦 Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
 # Inicia o Gunicorn
+echo "🚀 Iniciando o servidor Gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
